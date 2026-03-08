7bb5e06e7d0169ee13d9cd87be332739f209e380
Durgaprasad <121225776+Gdp-dp@users.noreply.github.com>
2026-03-08 22:40:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-var weddingDate = new Date("Dec 20, 2026 10:30:00").getTime();
-
-var x = setInterval(function(){
-
-var now = new Date().getTime();
-
-var distance = weddingDate - now;
-
-var days = Math.floor(distance / (1000*60*60*24));
-var hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
-var minutes = Math.floor((distance % (1000*60*60))/(1000*60));
-
-document.getElementById("countdown").innerHTML =
-days + " Days " + hours + " Hours " + minutes + " Minutes ";
-
-},1000);
+// Future animations or features can be added here
