c0197ee4c98ce430f7f893f239f0930f2591414a
Durgaprasad <121225776+Gdp-dp@users.noreply.github.com>
2026-03-14 14:20:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Future animations or features can be added here
+let celebrateBtn = document.getElementById('celebrate');
+let homePage = document.getElementById('pageHome');
+
+celebrateBtn.onclick = function () {
+    homePage.classList.add('bg1');
+    setTimeout(function () {
+        homePage.classList.remove('bg1');
+    }, 5000);
+}
